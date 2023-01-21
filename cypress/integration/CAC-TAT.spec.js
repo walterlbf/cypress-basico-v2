af0ff1840f84a4909170592ccc21e4b765b4503b
walter <walter.lbf.f@gmail.com>
2023-01-20 21:11:02 -0400

    be.equal fica mais semantico que include

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -7,6 +7,6 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
   it('verifica o título da aplicação', () => {
     cy.visit('src/index.html')
-    cy.title().should('include', 'Central de Atendimento ao Cliente TAT')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 })
