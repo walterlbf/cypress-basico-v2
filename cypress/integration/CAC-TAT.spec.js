e7eaf5b2b3b3b93e659c1c16a9cc7fe9cf580c4a
walter <walter.lbf.f@gmail.com>
2023-01-20 22:35:36 -0400

    teste que preenche os campos obrigatorios e envia o formulario

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -5,8 +5,20 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => {
     cy.visit('src/index.html')
-    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
+  
+    it('verifica o título da aplicação', () => {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('input[name="firstName"]').type('Walter')
+        cy.get('input[name="lastName"]').type('Luiz')
+        cy.get('input[type="email"]').type('walter@teste.com')
+        cy.get('textarea[name="open-text-area"]').type('Teste de envio de mensagem')
+        cy.get('button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+    })
 })
