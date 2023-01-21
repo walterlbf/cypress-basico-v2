677c8e11456fabb3965f8a607aaf631f3e409ca3
walter <walter.lbf.f@gmail.com>
2023-01-20 21:06:40 -0400

    exercicio 1 - feito

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,3 +4,9 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('src/index.html')
+    cy.title().should('include', 'Central de Atendimento ao Cliente TAT')
+  })
+})
